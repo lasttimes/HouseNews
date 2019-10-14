af1ace486acd7c4f90101442e6754ad540b83277
liujiane <lasttimes@163.com>
2019-10-14 11:22:16 +0800

    更新链接、正则

--- src/test/java/HouseUnitTest.java
+++ src/test/java/HouseUnitTest.java
@@ -25,14 +25,15 @@ public class HouseUnitTest {
         List<PathInfo> pathInfoList = Utils.loadPathList();
 
         for (PathInfo pathInfo : pathInfoList) {
+            logger.info(pathInfo.toString());
             String htmlBody = Utils.getHtmlBodyText(logger, pathInfo.url, pathInfo.charset);
             Pattern pattern = Pattern.compile(pathInfo.regex);
             if (htmlBody == null) {
                 logger.warn("htmlBody is null: " + pathInfo.url);
                 continue;
             }
+            logger.info(pathInfo.toString());
             Matcher m = pattern.matcher(htmlBody);
-            logger.info(pathInfo.name + " url:" + pathInfo.url);
             assertTrue(m.find());
             NewsInfo info = NewsInfo.getCreator(pathInfo.creator).create(m);
             logger.info("newsInfo:" + info);
@@ -48,10 +49,15 @@ public class HouseUnitTest {
 
     @Test
     public void temp() {
-        PathInfo pathInfo = Utils.loadPathList().get(0);
+        List<PathInfo> list = Utils.loadPathList();
+        PathInfo pathInfo = list.get(list.size() - 1);
+        logger.info(pathInfo.toString());
 
         String htmlBody = Utils.getHtmlBodyText(logger, pathInfo.url, pathInfo.charset);
         Pattern pattern = Pattern.compile(pathInfo.regex);
+        assertNotNull(htmlBody);
+        // test
+        htmlBody = "<a href=\"./201910/t20191008_18242711.htm\" title=\"深圳市大鹏新区住房和建设局关于面向大鹏新区先进制造业企业定向配租公共租赁住房的通告\">深圳市大鹏新区住房和建设局关于面向大鹏新区先进制造业企业定向配租公共租赁住房的通告</a></li>";
         Matcher m = pattern.matcher(htmlBody);
         while (m.find()){
             logger.info(pathInfo.name + " url:" + pathInfo.url);
